ac759f4be06f12aeff2db67a68a7093303fb08a8
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-14 06:06:02 +0900

    Remove skimage from dependencies

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from skimage import data
 import numpy as np
 from scipy.ndimage import map_coordinates
 from interpolation import interpolation
 
 
 def main():
-    image = data.coins().astype(np.float32)
-    height, width = image.shape
+    height, width = 300, 400
     N = 64
-    xs = np.random.uniform(0, height-1, N)
-    ys = np.random.uniform(0, width-1, N)
+    image = np.random.randint(0, 100, (height, width)).astype(np.float32)
+    xs = np.random.uniform(0, width-1, N)
+    ys = np.random.uniform(0, height-1, N)
     C = np.column_stack((xs, ys)).astype(np.float32)
     intensities = interpolation(image, C)
     print("interpolation result")
