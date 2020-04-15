3756d52fa3061ea0980fbec5c7d46ea371df056a
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-15 20:42:15 +0900

    Compile with '-Ofast' option

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ setup(
     author="Takeshi Ishita",
     author_email="ishitah.takeshi@gmail.com",
     ext_modules=[
-        Extension("interpolation",
-                  sources=["interpolation.pyx", "_bilinear.c"],
-                  include_dirs=[np.get_include()],
-                  extra_compile_args=["-g", "-Wall", "-mavx", "-mavx2"])
+        Extension(
+            "interpolation",
+            sources=["interpolation.pyx", "_bilinear.c"],
+            include_dirs=[np.get_include()],
+            extra_compile_args=["-g", "-Wall", "-mavx", "-mavx2", "-Ofast"])
     ]
 )
