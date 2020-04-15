ecceed182af938c0952220b5fd1840ef93bd8fa1
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-15 20:38:10 +0900

    Add an interface for the normal C implementation

--- interpolation.pyx
+++ interpolation.pyx
@@ -29,3 +29,21 @@ def interpolation_simd(cnp.ndarray[cnp.float32_t, ndim=2] image,
     _interpolation_simd(&image_view[0], width, &xs[0], &ys[0], N,
                         &intensities_view[0])
     return intensities
+
+
+def interpolation_normal(cnp.ndarray[cnp.float32_t, ndim=2] image,
+                         cnp.ndarray[cnp.float32_t, ndim=2] coordinates):
+
+    cdef float[:] image_view = image.flatten()
+    cdef int width = image.shape[1]
+
+    cdef float[:] xs = np.ascontiguousarray(coordinates[:, 0])
+    cdef float[:] ys = np.ascontiguousarray(coordinates[:, 1])
+
+    cdef int N = coordinates.shape[0]
+    intensities = np.empty(N, dtype=np.float32)
+
+    cdef float[:] intensities_view = intensities
+    _interpolation_normal(&image_view[0], width, &xs[0], &ys[0], N,
+                          &intensities_view[0])
+    return intensities
